f5a190b542200dbc059f7f6a921ffacaf85c682d
yixiaoweishuihuan <1069441413@qq.com>
2020-03-26 19:24:38 +0800

    用户页面完成 有一个报错不影响功能 待完善

---
+++ src/views/user/com/processUser.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <el-dialog :title="isEdit?'修改用户':'新增用户'" :visible.sync="dialogFormVisible">
+      <el-form :model="form" :rules="rules" ref="form">
+        <el-form-item label="用户名" prop="username" :label-width="formLabelWidth">
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email" :label-width="formLabelWidth">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="phone" :label-width="formLabelWidth">
+          <el-input v-model="form.phone" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="角色" prop="role_id" :label-width="formLabelWidth">
+          <el-select v-model="form.role_id">
+            <el-option label="请选择角色" value></el-option>
+            <el-option label="超级管理员" :value="1"></el-option>
+            <el-option label="管理员" :value="2"></el-option>
+            <el-option label="老师" :value="3"></el-option>
+            <el-option label="学生" :value="4"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="状态" prop="status" :label-width="formLabelWidth">
+          <el-select v-model="form.status">
+            <el-option label="请选择状态" value></el-option>
+            <el-option label="禁用" :value="0"></el-option>
+            <el-option label="启用" :value="1"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="用户备注" prop="remark" :label-width="formLabelWidth">
+          <el-input v-model="form.remark" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="cancel">取 消</el-button>
+        <el-button type="primary" @click="onSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+<script>
+//导入 自定义校验方法
+import { checkPhone, checkEmail } from "@/utils/mycheck.js";
+//导入 操作用户的api
+import { apiEditUser, apiAddUser } from "@/api/user.js";
+export default {
+  data() {
+    return {
+      isEdit: true, //是否是修改页面
+      dialogFormVisible: false, //是否显示面板
+      formLabelWidth: "122px",
+      form: {
+        id: "",
+        username: "",
+        email: "",
+        phone: "",
+        avatar: "",
+        password: "",
+        role_id: "",
+        status: "",
+        remark: "" //企业备注
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        email: [
+          { required: true, message: "请输入邮箱", trigger: "blur" },
+          { validator: checkEmail, trigger: "blur" }
+        ],
+        phone: [
+          { required: true, message: "请输入电话", trigger: "blur" },
+          { validator: checkPhone, trigger: "blur" }
+        ],
+        role_id: [{ required: true, message: "请选择角色", trigger: "blur" }],
+        status: [],
+        remark: []
+      }
+    };
+  },
+  methods: {
+    //确认按钮
+    onSubmit() {
+      if (this.isEdit) {
+        this.$refs.form.validate(valid => {
+          if (valid) {
+            apiEditUser(this.form)
+              .then(res => {
+                if (res.data.code == 200) {
+                  //隐藏面板
+                  this.dialogFormVisible = false;
+                  //刷新企业列表
+                  this.$parent.getUserList();
+                  this.$message.success("修改成功！");
+                } else {
+                  this.$message.error(res.data.message);
+                }
+              })
+              .catch(err => {
+                window.console.log(err);
+              });
+          } else {
+            this.$message.error("验证失败！");
+            return false;
+          }
+        });
+      } else {
+        this.$refs.form.validate(valid => {
+          if (valid) {
+            apiAddUser(this.form)
+              .then(res => {
+                if (res.data.code == 200) {
+                  //重新渲染企业列表
+                  this.$parent.getUserList();
+                  this.$message.success("新增成功！");
+                  //隐藏面板 清空表单
+                  this.cancel();
+                } else {
+                  this.$message.error(res.data.message);
+                }
+              })
+              .catch(err => {
+                window.console.log(err);
+              });
+          } else {
+            this.$message.error("验证失败！");
+          }
+        });
+      }
+    },
+    //取消按钮
+    cancel() {
+      if (!this.isEdit) {
+        //清空表单
+        this.$refs.form.resetFields();
+      }
+      //隐藏面板
+      this.dialogFormVisible = false;
+    }
+  }
+};
+</script>
+<style>
+</style>
--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+  <div>
     <!-- 顶部 -->
     <el-card>
       <el-form :inline="true" :model="obj" class="demo-form-inline">
@@ -63,76 +63,149 @@
         :total="counts"
       ></el-pagination>
     </el-card>
-</div>
+    <!-- 新增 / 编辑 用户的组件 -->
+    <processUser ref="processUser"></processUser>
+  </div>
 </template>
 <script>
+//导入 新增 / 编辑 用户的组件
+import processUser from "./com/processUser";
 //导入操作用户的方法api
-import { apiUserList } from '@/api/user.js'
+import { apiUserList, apiChangeStatus, apiDelUser } from "@/api/user.js";
 export default {
-    data() {
-        return {
-            //筛选使用数据
-            obj:{
-                username:'',
-                email:'',
-                role_id:'',//角色
-                page:1,//当前页
-                limit:4//当前页容量
-            },
-            //表格数据
-            userList:[],
-            //页容量分组
-            pageSizeArr:[2,4,8],
-            //数据总条数
-            counts:0,
-        }
+  components: {
+    processUser
+  },
+  data() {
+    return {
+      //筛选使用数据
+      obj: {
+        username: "",
+        email: "",
+        role_id: "", //角色
+        page: 1, //当前页
+        limit: 4 //当前页容量
+      },
+      //表格数据
+      userList: [],
+      //页容量分组
+      pageSizeArr: [2, 4, 8],
+      //数据总条数
+      counts: 0
+    };
+  },
+  methods: {
+    //获取用户列表
+    getUserList() {
+      apiUserList(this.obj)
+        .then(res => {
+          if (res.data.code == 200) {
+            this.userList = res.data.data.items;
+            this.counts = res.data.data.pagination.total;
+          } else {
+            this.$message.error(res.data.message);
+          }
+        })
+        .catch(err => {
+          window.console.log(err);
+        });
     },
-    methods: {
-        //获取用户列表
-        getUserList(){
-            apiUserList(this.obj).then(res=>{
-                if(res.data.code==200){
-                    this.userList = res.data.data.items;
-                    this.counts = res.data.data.pagination.total;
-                }else{
-                    this.$message.error(res.data.message);
+    //筛选用户
+    searchUser() {
+      this.getUserList();
+    },
+    //清除筛选
+    clearSearch() {
+      this.obj.username = "";
+      this.obj.email = "";
+      this.obj.role_id = "";
+    },
+    //新增用户
+    addUser() {
+      //清空表单
+      this.$refs.processUser.dialogFormVisible = true;
+      this.$refs.processUser.isEdit = false;
+      this.$refs.processUser.$refs.form.resetFields();
+    },
+    //编辑用户
+    editUser(row) {
+      this.$refs.processUser.dialogFormVisible = true;
+      this.$refs.processUser.isEdit = true;
+      //显示用户信息
+      if (row.id != this.$refs.processUser.form.id) {
+        this.$refs.processUser.form = JSON.parse(JSON.stringify(row));
+      }
+    },
+    //禁用/启用
+    disable(row) {
+      apiChangeStatus(row.id)
+        .then(res => {
+          if (res.data.code == 200) {
+            if (row.status == 0) {
+              this.$message.success("启用成功!");
+            } else {
+              this.$message.success("禁用成功!");
+            }
+            //刷新用户列表
+            this.getUserList();
+          } else {
+            this.$message.error(res.data.message);
+          }
+        })
+        .catch(err => {
+          window.console.log(err);
+        });
+    },
+    //删除用户
+    delate(row) {
+      this.$confirm("是否删除该用户?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          apiDelUser(row.id)
+            .then(res => {
+              if (res.data.code == 200) {
+                this.$message.success("删除成功!");
+                if (this.userList == 1) {
+                  this.obj.page--;
                 }
-            }).catch(err=>{
-                window.console.log(err);
+                //刷新用户列表
+                this.getUserList();
+              } else {
+                this.$message.error(res.data.message);
+              }
             })
-        },
-        //筛选用户
-        searchUser(){
-            this.getUserList();
-        },
-        //清除筛选
-        clearSearch(){},
-        //新增用户
-        addUser(){},
-        //编辑用户
-        editUser(){},
-        //禁用/启用
-        disable(){},
-        //删除用户
-        delate(){},
-        //改变页容量
-        sizeChange(index){
-            this.obj.limit = index;
-            //刷新用户列表
-            this.getUserList();
-        },
-        //改编页码
-        currentChange(index){
-            this.obj.page = index;
-            //刷新用户列表
-            this.getUserList();
-        },
+            .catch(err => {
+              window.console.log(err);
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
     },
-    created() {
-        //页面一加载 获取用户列表
-        this.getUserList();
+    //改变页容量
+    sizeChange(index) {
+      this.obj.limit = index;
+      //刷新用户列表
+      this.getUserList();
     },
-}
+    //改编页码
+    currentChange(index) {
+      this.obj.page = index;
+      //刷新用户列表
+      this.getUserList();
+    }
+  },
+  created() {
+    //页面一加载 获取用户列表
+    this.getUserList();
+  }
+};
 </script>
 <style>
 </style>
